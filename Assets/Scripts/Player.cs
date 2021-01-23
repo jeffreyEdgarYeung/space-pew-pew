99568d1ed55861ac07bedf04e5fbb1ac8568ae65
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-22 17:25:49 -0800

    Player movement boundries established

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField][Range(0,50)] float moveSpeed = 10f;
+    [SerializeField] float xPadding, yPadding;
 
+    float xMin, xMax, yMin, yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpBoundaries();
+    }
+
+    private void SetUpBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + xPadding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - xPadding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + yPadding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - yPadding;
     }
 
     // Update is called once per frame
@@ -22,10 +34,10 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos =Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
     }
