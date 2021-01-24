a2779e87ff18ee64a3e3b2e5a85cd88107ad0971
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-24 14:50:52 -0800

    Enemy health and damage processing

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer, collision);
+       
+    }
+
+    private void ProcessHit(DamageDealer damageDealer, Collider2D collision)
+    {
+        health -= damageDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "Laser")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
