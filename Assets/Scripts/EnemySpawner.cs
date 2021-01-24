ec2fe39fa2c8b6bc09522eb8cbfc00dea368c661
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-23 21:41:11 -0800

    Enemy Wave Decouple refactor

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int waveIdx = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currWave = waveConfigs[waveIdx];
+        StartCoroutine(SpawnWaveEnemies(currWave));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnWaveEnemies(WaveConfig currWave)
+    {
+        for( var i=0; i<currWave.GetNumberOfEnemies(); i++)
+        {
+            var newEnemy = Instantiate
+            (
+                currWave.GetEnemyPrefab(),
+                currWave.GetWaypoints()[0].transform.position,
+                currWave.GetEnemyPrefab().transform.rotation
+            );
+
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(currWave);
+            yield return new WaitForSeconds(currWave.GetTimeBetweenSpawns());
+        }
+        
+    }
+}
