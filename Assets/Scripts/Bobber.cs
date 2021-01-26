f284606bfecae420dc07a6897f94607efc0dcc78
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-01-26 00:01:50 -0800

    Start and game over scenes

---
+++ Assets/Scripts/Bobber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bobber : MonoBehaviour
+{
+    [SerializeField] float floatStrength;
+    Vector2 bobPosition;
+    float originalY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bobPosition = transform.position;
+        bobPosition.y = originalY + (Mathf.Sin(Time.time) * floatStrength);
+        transform.position = bobPosition;
+    }
+}
